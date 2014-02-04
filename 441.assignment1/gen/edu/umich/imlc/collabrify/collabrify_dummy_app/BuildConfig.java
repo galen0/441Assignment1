6d1073031207f2bcbc8d1d2c1f6f7e9a43e579e6
galen0 <ggong@umich.edu>
2014-02-03 19:34:53 -0500

    Removed old code, added collabrify demo

---
+++ 441.assignment1/gen/edu/umich/imlc/collabrify/collabrify_dummy_app/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.umich.imlc.collabrify.collabrify_dummy_app;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
